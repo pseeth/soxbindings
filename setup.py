9e5f38479817bd30bf5dd5e8d03125e02b9e39e0
pseeth <prem@descript.com>
2020-09-23 15:43:09 -0700

    Small fix to get format list on machines where sox isn't installed.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools.command.build_ext import build_ext
 import sys
 import setuptools
 
-__version__ = '1.2.0'
+__version__ = '1.2.1'
 
 
 class get_pybind_include(object):
--- soxbindings/transform.py
+++ soxbindings/transform.py
@@ -1,6 +1,23 @@
 from sox import Transformer as BaseTransformer
 from sox.transform import ENCODINGS_MAPPING
 
+# Need a hack for machines where the sox binary doesn't exist.
+# But SoxBindings will still work.
+import sox as pysox
+pysox.transform.VALID_FORMATS = [
+    '8svx', 'aif', 'aifc', 'aiff', 'aiffc', 'al', 'amb', 'au', 
+    'avr', 'caf', 'cdda', 'cdr', 'cvs', 'cvsd', 'cvu', 'dat', 
+    'dvms', 'f32', 'f4', 'f64', 'f8', 'fap', 'flac', 'fssd', 
+    'gsm', 'gsrt', 'hcom', 'htk', 'ima', 'ircam', 'la', 'lpc', 
+    'lpc10', 'lu', 'mat', 'mat4', 'mat5', 'maud', 'mp2', 'mp3', 
+    'nist', 'ogg', 'opus', 'paf', 'prc', 'pvf', 'raw', 's1', 
+    's16', 's2', 's24', 's3', 's32', 's4', 's8', 'sb', 'sd2', 
+    'sds', 'sf', 'sl', 'sln', 'smp', 'snd', 'sndfile', 'sndr', 
+    'sndt', 'sou', 'sox', 'sph', 'sw', 'txw', 'u1', 'u16', 'u2', 
+    'u24', 'u3', 'u32', 'u4', 'u8', 'ub', 'ul', 'uw', 'vms', 'voc', 
+    'vorbis', 'vox', 'w64', 'wav', 'wavpcm', 'wve', 'xa', 'xi'
+]
+
 from sox.log import logger
 from .sox_cli import sox
 from .audio import get_info
