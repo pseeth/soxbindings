bd3277ba42c71778be868c0162707bb175972c6f
pseeth <prem@descript.com>
2021-02-25 22:27:25 -0800

    Adding --disable-open-mp. Adding mutlithread test.

---
+++ tests/test_multithread.py
@@ -0,0 +1,27 @@
+from multiprocessing.dummy import Pool as ThreadPool
+import numpy as np
+import soxbindings as sox
+
+def test_multithreading():
+    y1 = np.zeros((4000, 1))
+    y2 = np.zeros((3000, 1))
+
+    def do_transform(y):
+        tfm = sox.Transformer()
+        tfm.vol(0.5)
+        y_out = tfm.build_array(input_array=y, sample_rate_in=1000)
+        return y_out
+
+    # single thread
+    single_thread = []
+    for y in [y1, y2]:
+        res = do_transform(y)
+        single_thread.append(res)
+
+    # multithread
+    pool = ThreadPool(2)
+    multi_thread = pool.map(do_transform, [y1, y2])
+
+    for a1, a2 in zip(single_thread, multi_thread):
+        assert np.allclose(a1, a2)
+    
